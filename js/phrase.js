23b6e06c244b76eb34032228070841f1d4b11d5b
yury-kalinkin <yury.kalinkin@ardas.dp.ua>
2017-05-21 01:48:35 +0300

    Dialog v.0.2

---
+++ js/phrase.js
@@ -0,0 +1,6 @@
+mxmz.Dialog = function (maxPhrase, npcAnswer, result, condition) {
+    this.maxPhrase = maxPhrase;
+    this.npcAnswer = npcAnswer;
+    this.result = result;
+    this.condition = condition;
+}
